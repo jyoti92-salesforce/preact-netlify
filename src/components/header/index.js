5f6feeef126e8381bc6201b56e5629084d69b427
jyoti92-salesforce <67104013+jyoti92-salesforce@users.noreply.github.com>
2021-04-17 08:41:12 +0530

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,6 +8,7 @@ const Header = () => (
 		<nav>
 			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
 			<Link activeClassName={style.active} href="/contact">Contact me</Link>
+	
 		</nav>
 	</header>
 );
